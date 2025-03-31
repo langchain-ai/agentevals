a9e7847d3c102c99d1e2339d941dce4176f4de9d
jacoblee93 <jacoblee93@gmail.com>
2025-03-31 13:08:37 -0700

    Fix test case

--- python/tests/test_trajectory_async.py
+++ python/tests/test_trajectory_async.py
@@ -670,7 +670,7 @@ async def test_trajectory_match_with_langchain_messages_failure(
         ("strict", False),
     ],
 )
-def test_trajectory_match_with_overrides(trajectory_match_mode, score):
+async def test_trajectory_match_with_overrides(trajectory_match_mode, score):
     outputs = [
         {"role": "user", "content": "Hi there, what time is my flight?"},
         {
@@ -881,7 +881,7 @@ def test_trajectory_match_with_overrides(trajectory_match_mode, score):
         trajectory_match_mode=trajectory_match_mode,
     )
 
-    evaluator_no_overrides_result = evaluator_no_overrides(
+    evaluator_no_overrides_result = await evaluator_no_overrides(
         outputs=outputs, reference_outputs=reference_outputs
     )
     assert not evaluator_no_overrides_result["score"]
@@ -915,7 +915,7 @@ def test_trajectory_match_with_overrides(trajectory_match_mode, score):
         ("strict"),
     ],
 )
-def test_trajectory_match_with_nested_field_overrides(trajectory_match_mode):
+async def test_trajectory_match_with_nested_field_overrides(trajectory_match_mode):
     outputs = [
         {"role": "user", "content": "Hi there, what time is my flight?"},
         {
@@ -1098,7 +1098,7 @@ def test_trajectory_match_with_nested_field_overrides(trajectory_match_mode):
         trajectory_match_mode=trajectory_match_mode,
     )
 
-    evaluator_no_overrides_result = evaluator_no_overrides(
+    evaluator_no_overrides_result = await evaluator_no_overrides(
         outputs=outputs, reference_outputs=reference_outputs
     )
     assert not evaluator_no_overrides_result["score"]
